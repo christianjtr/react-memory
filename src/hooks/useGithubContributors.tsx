696b6cfb60502c687e5b670b4055366663657a37
Christian José Torrealba Rondón <torrealba.christian@gmail.com>
2022-09-25 21:43:21 +0200

    Enhance hook definition

--- src/hooks/useGithubContributors.tsx
+++ src/hooks/useGithubContributors.tsx
@@ -7,17 +7,17 @@ import { adaptGithubContributors } from '../adapters/contributor.adapter';
 interface GithubContributorHookInterface {
   isLoading: boolean;
   contributors: Contributor[];
-  fetchData: (owner?: string, repositoryName?: string) => void;
+  fetchData: (repository?: { owner: string; name: string }) => void;
 }
 
 const useGithubContributors = (): GithubContributorHookInterface => {
   const [isLoading, setIsLoading] = useState<boolean>(false);
   const [contributors, setContributors] = useState<Contributor[]>([]);
 
-  async function fetchData(owner?: string, repositoryName?: string): Promise<void> {
+  async function fetchData(repository?: { owner: string; name: string }): Promise<void> {
     setIsLoading(true);
 
-    const data = await fetchRepositoryContributors(owner, repositoryName);
+    const data = await fetchRepositoryContributors(repository?.owner, repository?.name);
 
     setContributors(adaptGithubContributors(data));
     setIsLoading(false);
